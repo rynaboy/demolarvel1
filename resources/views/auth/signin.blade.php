64fd8b451953efedb2c7e8ea873db73bbf71fd45
cana_trust <youremail@example.com>
2024-09-02 21:44:57 +0700

    first commit

---
+++ resources/views/auth/signin.blade.php
@@ -0,0 +1,79 @@
+<x-guest-layout>
+    <main class="main-content  mt-0">
+        <section>
+            <div class="page-header min-vh-100">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-4 col-md-6 d-flex flex-column mx-auto">
+                            <div class="card card-plain mt-8">
+                                <div class="card-header pb-0 text-left bg-transparent text-center">
+                                    <h3 class="font-weight-black text-dark display-6">Welcome back</h3>
+                                </div>
+                                <div class="text-center">
+                                    @if (session('status'))
+                                        <div class="mb-4 font-medium text-sm text-green-600">
+                                            {{ session('status') }}
+                                        </div>
+                                    @endif
+                                    @error('message')
+                                        <div class="alert alert-danger text-sm" role="alert">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+                                <div class="card-body">
+                                    <form role="form" class="text-start" method="POST" action="sign-in">
+                                        @csrf
+                                        <label>Email Address</label>
+                                        <div class="mb-3">
+                                            <input type="email" id="email" name="email" class="form-control"
+                                                placeholder="Enter your email address"
+                                                value="{{ old('email') ? old('email') : 'admin@corporateui.com' }}"
+                                                aria-label="Email" aria-describedby="email-addon">
+                                        </div>
+                                        <label>Password</label>
+                                        <div class="mb-3">
+                                            <input type="password" id="password" name="password"
+                                                value="{{ old('password') ? old('password') : 'secret' }}"
+                                                class="form-control" placeholder="Enter password" aria-label="Password"
+                                                aria-describedby="password-addon">
+                                        </div>
+                                        <div class="d-flex align-items-center">
+                                            <div class="form-check form-check-info text-left mb-0">
+                                                <input class="form-check-input" type="checkbox" value=""
+                                                    id="flexCheckDefault">
+                                                <label class="font-weight-normal text-dark mb-0" for="flexCheckDefault">
+                                                    Remember for 14 days
+                                                </label>
+                                            </div>
+                                            <a href="{{ route('password.request') }}"
+                                                class="text-xs font-weight-bold ms-auto">Forgot
+                                                password</a>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-dark w-100 mt-4 mb-3">Sign in</button>
+                                        </div>
+                                    </form>
+                                </div>
+                                <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                                    <p class="mb-4 text-xs mx-auto">
+                                        Don't have an account?
+                                        <a href="{{ route('sign-up') }}" class="text-dark font-weight-bold">Sign up</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div
+            class="blur mt-12 text-center border border-white border-radius-md position-absolute fixed-bottom m-4">
+            <h6 class="text-dark text-sm mt-5">Copyright © <script>
+                    document.write(new Date().getFullYear())
+                </script>
+                by TSD Co,.Ltd.</h6>
+        </div>
+        </section>
+    </main>
+
+</x-guest-layout>
---
+++ resources/views/components/app/footer.blade.php
@@ -0,0 +1,14 @@
+<footer class="footer p-5 ">
+    <div class="row align-items-center justify-content-lg-between ">
+        <div class="col-lg-6 mb-lg-0 mb-4">
+            <div class="copyright text-center text-m text-muted text-lg-start">
+                Copyright
+                ©
+                <script>
+                    document.write(new Date().getFullYear())
+                </script>
+                made with <i class="fa fa-heart"></i> by TSD Co,.Ltd.
+            </div>
+        </div>
+    </div>
+</footer>
